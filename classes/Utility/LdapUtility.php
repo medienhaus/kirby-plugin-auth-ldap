0174631071e86f788ca2155d4c51bf3f98b75510
Andi Rueckel <andirueckel@users.noreply.github.com>
2025-01-16 16:19:06 +0100

    chore: formatting

--- classes/Utility/LdapUtility.php
+++ classes/Utility/LdapUtility.php
@@ -1,6 +1,7 @@
 <?php
 
-class LdapUtility {
+class LdapUtility
+{
     private $ldapConn = null;
     private static $utility = null;
 
@@ -8,7 +9,8 @@ class LdapUtility {
      * LdapUtility constructor.
      * stores itself in a static variable
      */
-    function __construct() {
+    public function __construct()
+    {
         global $utility;
         $utility = $this;
     }
@@ -19,7 +21,8 @@ class LdapUtility {
      *
      * @return LdapUtility
      */
-    public static function getUtility() {
+    public static function getUtility()
+    {
         global $utility;
         if ($utility == null) {
             return new LdapUtility();
@@ -35,27 +38,30 @@ class LdapUtility {
      *
      * @return array|false
      */
-    public function getLdapUser($mail) {
+    public function getLdapUser($mail)
+    {
         $ldap_base_dn = option('datamints.ldap.base_dn');
-        if(empty($mail)) return false;
+        if (empty($mail)) {
+            return false;
+        }
 
         $ldap = $this->getLdapConnection();
 
         //search for matching user
         $filter = "(mail=$mail)";
         $result = ldap_search($ldap, $ldap_base_dn, $filter);
-        
+
         //get user
         $entries = ldap_get_entries($ldap, $result);
-        
+
         //create user object. Is false on fail.
         $user = false;
-        
+
         //check if user is found
         $count = $entries["count"];
-        if(0<$count) {
+        if (0 < $count) {
             $entry = $entries[0];
-            
+
             //beautify user-array
             $user = [
                 "uid" => $entry["uid"][0],
@@ -66,7 +72,7 @@ class LdapUtility {
                 "mail" => $entry["mail"][0],
             ];
         }
-        
+
         //return user array or false on fail
         return $user;
     }
@@ -77,9 +83,12 @@ class LdapUtility {
      *
      * @return mixed
      */
-    private function getLdapConnection() {
+    private function getLdapConnection()
+    {
         global $ldapConn;
-        if($ldapConn != null) return $ldapConn;
+        if ($ldapConn != null) {
+            return $ldapConn;
+        }
         $this->getNewLdapConnection();
         return $ldapConn;
     }
@@ -88,7 +97,8 @@ class LdapUtility {
      * Creates a new LdapConnection generated with ldap_connect(), sets options, starts tls and binds it once that ldap_search works.
      * Sets the new Connection into the global variable $ldapConn
      */
-    private function getNewLdapConnection() {
+    private function getNewLdapConnection()
+    {
         global $ldapConn;
         $ldap_host = option("datamints.ldap.host");
         $ldap_bind_dn = option("datamints.ldap.bind_dn");
@@ -96,20 +106,20 @@ class LdapUtility {
 
         //create uri-element
         //TODO or throw Error
-        $ldapConn = ldap_connect($ldap_host) or die("invalid Host: ".$ldap_host." - it should look like ldap://subdomain.domain.tld:port");
+        $ldapConn = ldap_connect($ldap_host) or die("invalid Host: " . $ldap_host . " - it should look like ldap://subdomain.domain.tld:port");
 
         //Ldap-options
         ldap_set_option($ldapConn, LDAP_OPT_PROTOCOL_VERSION, 3);
         ldap_set_option($ldapConn, LDAP_OPT_REFERRALS, 0);
 
         // turn on detailed debugging for ldap if debug is set to true
-        if(option('debug') === true) {
-            ldap_set_option(NULL, LDAP_OPT_DEBUG_LEVEL, 7);
+        if (option('debug') === true) {
+            ldap_set_option(null, LDAP_OPT_DEBUG_LEVEL, 7);
         }
 
         // only upgrade to TLS, if configured in the environment
         if (option('start_tls') === true) {
-            ldap_start_tls($ldapConn) or die("cant connect tls: ".$ldap_host);
+            ldap_start_tls($ldapConn) or die("cant connect tls: " . $ldap_host);
         }
         //bind Ldap-server
         $this->getLdapBind($ldap_bind_dn, $ldap_bind_pw);
@@ -123,8 +133,9 @@ class LdapUtility {
      * @param string $password
      * @return bool
      */
-    private function getLdapBind($user, $password) {
-        set_error_handler(function() { $bind = false; });
+    private function getLdapBind($user, $password)
+    {
+        set_error_handler(function () { $bind = false; });
         $bind = ldap_bind($this->getLdapConnection(), $user, $password);
         restore_error_handler();
         return $bind;
@@ -138,8 +149,11 @@ class LdapUtility {
      * @return string
      * @throws Exception
      */
-    public function getLdapDn($mail) {
-        if(strlen($mail)<1) throw new Exception("get Ldap DN without mail");
+    public function getLdapDn($mail)
+    {
+        if (strlen($mail) < 1) {
+            throw new Exception("get Ldap DN without mail");
+        }
         $user = $this->getLdapUser($mail);
         return $user["dn"];
     }
@@ -154,14 +168,16 @@ class LdapUtility {
      * @return bool
      * @throws Exception
      */
-    public function validatePassword($mail, $ldap_user_pw) {
-        if(strlen($mail)<1) throw new Exception("validate Password without mail");
+    public function validatePassword($mail, $ldap_user_pw)
+    {
+        if (strlen($mail) < 1) {
+            throw new Exception("validate Password without mail");
+        }
         $ldap_user_dn = $this->getLdapDn($mail);
         $bind = $this->getLdapBind($ldap_user_dn, $ldap_user_pw);
-        if($bind != false) {
+        if ($bind != false) {
             $bind = true;
         }
         return $bind;
     }
 }
-?>
